a1b6ac49c48ea2fb13c0201696e3a06b5ca2a2ac
ChugxScript <arw.oloroso@gmail.com>
2024-04-25 21:11:09 +0800

    [fix] not showing board

--- states/wumpus_world.py
+++ states/wumpus_world.py
@@ -46,7 +46,7 @@ class WumpusWorld():
             self.board_size = 4
             self.cell_size = 125
             self.board_coord = (80, 50)
-            self.probability_blue_cells = 0.5 # 0.2 is too low
+            self.probability_blue_cells = 0.6 # 0.2 is too low
             self.red_cells = 1
             self.yellow_cells = 1
         elif self.current_world_size == 'select_world_size_2':
@@ -67,17 +67,17 @@ class WumpusWorld():
             self.board_size = 10
             self.cell_size = 50
             self.board_coord = (82, 50)
-            self.probability_blue_cells = 0.2
+            self.probability_blue_cells = 2
             self.red_cells = 6
             self.yellow_cells = 3
         else:
             print(f"Invalid self.current_world_size: {self.current_world_size}")
 
     def draw_board(self):
-        # self.cell_size = 50
         self.board_width = self.board_size * self.cell_size
         self.board_height = self.board_size * self.cell_size
         self.board_surface = pygame.Surface((self.board_width, self.board_height))
+        print("--- 1")
         
         # the first square is the starting position therefore the color is white
         # the number of blue, red, and yellow will depend on the size of board_size
@@ -89,6 +89,7 @@ class WumpusWorld():
                 row.append(0)  # 0 represents white cells
             self.cells.append(row)
         
+        print("--- 2")
         # check if there is a path from first cell to the yellow cells in the board where the path must not go through the blue or red cells
         # if there are no path from first cell to the yellow cells then make a random path from first cell to the yellow cells
         # by making the random path cells white
@@ -105,6 +106,7 @@ class WumpusWorld():
                 if self.cells[y][x] == 0 and not (y == 0 and x == 1) and not (y == 1 and x == 0):
                     self.cells[y][x] = 2  # 2 represents blue cells
                     break
+        print("--- 3")
 
         # Place red cells
         red_cells_placed = 0
@@ -115,6 +117,8 @@ class WumpusWorld():
                 if self.cells[y][x] == 0 and not (y == 0 and x == 1) and not (y == 1 and x == 0):  
                     self.cells[y][x] = 3  # 3 represents red cells
                     red_cells_placed += 1
+                    break
+        print("--- 4")
 
         # Place yellow cells
         for _ in range(self.yellow_cells):
@@ -124,6 +128,7 @@ class WumpusWorld():
                 if abs(x - start_x) + abs(y - start_y) > 2 and self.cells[y][x] == 0:  # Only place if cell is empty and not too close to the starting position
                     self.cells[y][x] = 4  # 4 represents yellow cells
                     break
+        print("--- 5")
 
         # Check if there's a path from the starting cell to each yellow cell
         for y in range(self.board_size):
@@ -134,6 +139,7 @@ class WumpusWorld():
                         self.cells[start_y][start_x] = 1
                         self.draw_board()  # Restart drawing board if path is not valid
                         return
+        print("--- 6")
 
         # after having a valid path 
         # put black cell if the cell is the starting cell
@@ -144,63 +150,64 @@ class WumpusWorld():
         # put gray cell if the cell is adjacent to blue and yellow cell
         # put Turquoise cell if the cell is adjacent to red and yellow cell
         # put brown cell if the cell is adjacent to blue and red and yellow cell
-        # put Cyan cell if the cell is not adjacent to red, blue, and yellow
+        # put Cyan cell if the cell is not adjacent to red, blue, and yellow cell
 
         # Draw cells on the board
-        for y in range(self.board_size):
-            for x in range(self.board_size):
-                color = (255, 255, 255)  # Default color is white
-                if self.cells[y][x] == 1:  # Starting position
-                    color = (0, 0, 0)
-                elif self.cells[y][x] == 2:  # Blue cell
-                    color = (0, 0, 255)
-                elif self.cells[y][x] == 3:  # Red cell
-                    color = (255, 0, 0)
-                elif self.cells[y][x] == 4:  # Yellow cell
-                    color = (255, 255, 0)
-                pygame.draw.rect(self.board_surface, color, (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
-
         # for y in range(self.board_size):
         #     for x in range(self.board_size):
         #         color = (255, 255, 255)  # Default color is white
-        #         adjacent_colors = set()
         #         if self.cells[y][x] == 1:  # Starting position
-        #             color = (0, 0, 0)  # Black
+        #             color = (0, 0, 0)
         #         elif self.cells[y][x] == 2:  # Blue cell
         #             color = (0, 0, 255)
         #         elif self.cells[y][x] == 3:  # Red cell
         #             color = (255, 0, 0)
         #         elif self.cells[y][x] == 4:  # Yellow cell
         #             color = (255, 255, 0)
-
-        #         # Check adjacent cells
-        #         if y > 0:
-        #             adjacent_colors.add(self.cells[y - 1][x])
-        #         if y < self.board_size - 1:
-        #             adjacent_colors.add(self.cells[y + 1][x])
-        #         if x > 0:
-        #             adjacent_colors.add(self.cells[y][x - 1])
-        #         if x < self.board_size - 1:
-        #             adjacent_colors.add(self.cells[y][x + 1])
-
-        #         if 2 in adjacent_colors and 3 in adjacent_colors:
-        #             color = (255, 192, 203)  # Pink
-        #         elif 2 in adjacent_colors and 4 in adjacent_colors:
-        #             color = (128, 128, 128)  # Gray
-        #         elif 3 in adjacent_colors and 4 in adjacent_colors:
-        #             color = (64, 224, 208)  # Turquoise
-        #         elif 2 in adjacent_colors and 3 in adjacent_colors and 4 in adjacent_colors:
-        #             color = (165, 42, 42)  # Brown
-        #         elif 2 in adjacent_colors:
-        #             color = (0, 128, 0)  # Green
-        #         elif 3 in adjacent_colors:
-        #             color = (255, 165, 0)  # Orange
-        #         elif 4 in adjacent_colors:
-        #             color = (128, 0, 128)  # Purple
-        #         else:
-        #             color = (0, 255, 255)  # Cyan
-
         #         pygame.draw.rect(self.board_surface, color, (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
+
+        for y in range(self.board_size):
+            for x in range(self.board_size):
+                color = (255, 255, 255)  # Default color is white
+                adjacent_colors = set()
+                if self.cells[y][x] == 1:  # Starting position
+                    color = (0, 0, 0)  # Black
+                elif self.cells[y][x] == 2:  # Blue cell
+                    color = (0, 0, 255)
+                elif self.cells[y][x] == 3:  # Red cell
+                    color = (255, 0, 0)
+                elif self.cells[y][x] == 4:  # Yellow cell
+                    color = (255, 255, 0)
+
+                # Check adjacent cells
+                if y > 0:
+                    adjacent_colors.add(self.cells[y - 1][x])
+                if y < self.board_size - 1:
+                    adjacent_colors.add(self.cells[y + 1][x])
+                if x > 0:
+                    adjacent_colors.add(self.cells[y][x - 1])
+                if x < self.board_size - 1:
+                    adjacent_colors.add(self.cells[y][x + 1])
+
+                if 2 in adjacent_colors and 3 in adjacent_colors:
+                    color = (255, 192, 203)  # Pink
+                elif 2 in adjacent_colors and 4 in adjacent_colors:
+                    color = (128, 128, 128)  # Gray
+                elif 3 in adjacent_colors and 4 in adjacent_colors:
+                    color = (64, 224, 208)  # Turquoise
+                elif 2 in adjacent_colors and 3 in adjacent_colors and 4 in adjacent_colors:
+                    color = (165, 42, 42)  # Brown
+                elif 2 in adjacent_colors:
+                    color = (0, 128, 0)  # Green
+                elif 3 in adjacent_colors:
+                    color = (255, 165, 0)  # Orange
+                elif 4 in adjacent_colors:
+                    color = (128, 0, 128)  # Purple
+                else:
+                    color = (255, 255, 255)  # white
+
+                pygame.draw.rect(self.board_surface, color, (x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size))
+        print("--- 7")
 
     def is_valid_path(self, start_x, start_y, end_x, end_y):
         visited = [[False for _ in range(self.board_size)] for _ in range(self.board_size)]
